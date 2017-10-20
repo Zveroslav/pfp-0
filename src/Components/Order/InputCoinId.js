4de481b41f55582c5a7ad4f5cfdeda4bb3e0f87f
User <zveroslav@yahoo.com>
2017-10-20 10:28:57 +0300

    Add function for send ajax

--- src/Components/Order/InputCoinId.js
+++ src/Components/Order/InputCoinId.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import {packageQuery} from '../../utils.js'
 
 class InputCoinId extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {coinyId: ''};
+      Poster.on('afterPopupClosed', () => {
+        this.setState({coinyId:""})
+      });
+  }
+  setCoinyId = (e) => {
+    this.setState({coinyId:e.target.value})
+  }
+  sendOrderToCoinyPay = () => {
+    let query = packageQuery(this.props.order, Poster.settings, this.state.coinyId);
+    console.log(query);
+    Poster.makeRequest('https://zed.coinypay.com/payapi', {
+      method: 'post',
+      data: query,
+      timeout: 10000
+    }, (answer) => {
+      if (answer && Number(answer.status) === 200) {
+          console.log(answer);
+      } else {
+        console.log(answer);
+      }
+    });
+
+  }
   render () {
-    console.log(this.props.order)
     return(
       <div id="CoinyPay">
         <h3>Введите CoinyID</h3>
         <div id="body">
           <div>
-            <input className="inputId" />
+            <input className="inputId" value={this.state.coinyId} onChange={this.setCoinyId}/>
           </div>
-          <button className="btn-green pay">Оплатить</button>
+          <button className="btn-green pay" onClick={this.sendOrderToCoinyPay}>Оплатить</button>
         </div>
         <div id="info">
         <p>Введите CoinyID клиента и отслеживайте статус оплаты в таблице заказов CoinyPay.</p>
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,25 @@
-function sendQueryToCoinyPay(id) {
+export  const packageQuery = (order, settings, id) => {
+let content = [];
+console.log(Poster);
+for (var item in order.products) {
 
+    content.push(order.products[item].id)
+
+}
+console.log(content);
+
+Poster.makeApiRequest('menu.getProduct', {
+  method: 'get',
+  product_id: content[1],
+},
+  (product) => {
+    console.log(product);
+}
+)
+
+
+return {seller:settings.accountUrl, coiny_id:id, innerid: order.id, amount:order.subtotal, currency:settings.currencyCodeIso, description:order.comment, content:content}
+// 0) accountUrl в seller - это единственный уникальный идентификатор заведения которы есть ресторана (или его сети), как такого id самого заведения не нашел.
+// 1) Подпись удалена так как она будет отправленна через заголовок (по договоренностью с Poster)
+// 2) Вместо названия блюд в запросе ( свойство объекта content) будет отправляться id товаров из системы Poster, так-как пока что не найден способ поиска товаров по ID из Poster API.
 }
